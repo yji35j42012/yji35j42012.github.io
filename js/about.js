eacb026b0bbfd41204e8f6dad46947c7b027cf50
yji35j42012 <35093568+yji35j42012@users.noreply.github.com>
2019-12-23 13:17:17 +0800

    2019-12-13

--- js/about.js
+++ js/about.js
@@ -6,7 +6,6 @@ var about_ctrl = document.querySelectorAll('.about_ctrl div')
         var attentively_item = document.querySelector('.item_group #attentively_item')
 
         function aboutclickHendler() {
-            
             switch (this.id) {
                 case 'attentively':
                     console.log(this.id);
@@ -31,7 +30,6 @@ var about_ctrl = document.querySelectorAll('.about_ctrl div')
         })
 
         function remove_item_active() {
-            
             for (let i = 0; i < item_active.length; i++) {
                 const element = item_active[i];
                 element.classList.remove('active')
